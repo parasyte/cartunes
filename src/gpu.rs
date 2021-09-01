38733e0dbf71837ed695d8d88fef3006fb1727d3
Jay Oster <jay@kodewerx.org>
2021-08-31 23:37:43 -0700

    Update dependencies
    - Also fix some remaining issues with windows_subsystem and env_logger

--- src/gpu.rs
+++ src/gpu.rs
@@ -8,14 +8,12 @@ pub(crate) enum Error {
     /// No suitable [`wgpu::Adapter`] found
     #[error("No suitable `wgpu::Adapter` found.")]
     AdapterNotFound,
-
     /// Equivalent to [`wgpu::RequestDeviceError`]
     #[error("No wgpu::Device found.")]
     DeviceNotFound(wgpu::RequestDeviceError),
-
-    /// Equivalent to [`wgpu::SwapChainError`]
-    #[error("The GPU failed to acquire a swapchain frame.")]
-    Swapchain(wgpu::SwapChainError),
+    /// Equivalent to [`wgpu::SurfaceError`]
+    #[error("The GPU failed to acquire a surface frame.")]
+    Surface(wgpu::SurfaceError),
 }
 
 pub(crate) struct Gpu {
@@ -23,7 +21,6 @@ pub(crate) struct Gpu {
     pub(crate) queue: wgpu::Queue,
     surface: wgpu::Surface,
     window_size: winit::dpi::PhysicalSize<u32>,
-    swap_chain: wgpu::SwapChain,
 }
 
 impl Gpu {
@@ -31,63 +28,59 @@ impl Gpu {
         window: &W,
         window_size: winit::dpi::PhysicalSize<u32>,
     ) -> Result<Self, Error> {
-        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
         let surface = unsafe { instance.create_surface(window) };
         let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
             compatible_surface: Some(&surface),
             power_preference: wgpu::PowerPreference::HighPerformance,
         });
         let adapter = pollster::block_on(adapter).ok_or(Error::AdapterNotFound)?;
-        let (mut device, queue) =
+        let (device, queue) =
             pollster::block_on(adapter.request_device(&wgpu::DeviceDescriptor::default(), None))
                 .map_err(Error::DeviceNotFound)?;
-        let swap_chain = create_swap_chain(
-            &mut device,
-            &surface,
-            wgpu::TextureFormat::Bgra8UnormSrgb,
-            wgpu::PresentMode::Fifo,
-            &window_size,
-        );
 
-        Ok(Self {
+        let gpu = Self {
             device,
             queue,
             surface,
             window_size,
-            swap_chain,
-        })
+        };
+        gpu.reconfigure_surface();
+
+        Ok(gpu)
     }
 
-    fn recreate_swap_chain(&mut self) {
-        self.swap_chain = create_swap_chain(
-            &mut self.device,
-            &self.surface,
-            wgpu::TextureFormat::Bgra8UnormSrgb,
-            wgpu::PresentMode::Fifo,
-            &self.window_size,
-        );
+    fn reconfigure_surface(&self) {
+        self.surface.configure(
+            &self.device,
+            &wgpu::SurfaceConfiguration {
+                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                format: wgpu::TextureFormat::Bgra8UnormSrgb,
+                width: self.window_size.width,
+                height: self.window_size.height,
+                present_mode: wgpu::PresentMode::Fifo,
+            },
+        )
     }
 
     pub(crate) fn resize(&mut self, window_size: winit::dpi::PhysicalSize<u32>) {
         self.window_size = window_size;
-        self.recreate_swap_chain();
+        self.reconfigure_surface();
     }
 
-    pub(crate) fn prepare(
-        &mut self,
-    ) -> Result<(wgpu::CommandEncoder, wgpu::SwapChainFrame), Error> {
+    pub(crate) fn prepare(&mut self) -> Result<(wgpu::CommandEncoder, wgpu::SurfaceFrame), Error> {
         let frame = self
-            .swap_chain
+            .surface
             .get_current_frame()
             .or_else(|err| match err {
-                wgpu::SwapChainError::Outdated => {
+                wgpu::SurfaceError::Outdated => {
                     // Recreate the swap chain to mitigate race condition on drawing surface resize.
-                    self.recreate_swap_chain();
-                    self.swap_chain.get_current_frame()
+                    self.reconfigure_surface();
+                    self.surface.get_current_frame()
                 }
                 err => Err(err),
             })
-            .map_err(Error::Swapchain)?;
+            .map_err(Error::Surface)?;
         let encoder = self
             .device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor {
@@ -96,23 +89,4 @@ impl Gpu {
 
         Ok((encoder, frame))
     }
-}
-
-fn create_swap_chain(
-    device: &mut wgpu::Device,
-    surface: &wgpu::Surface,
-    format: wgpu::TextureFormat,
-    present_mode: wgpu::PresentMode,
-    surface_size: &winit::dpi::PhysicalSize<u32>,
-) -> wgpu::SwapChain {
-    device.create_swap_chain(
-        surface,
-        &wgpu::SwapChainDescriptor {
-            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
-            format,
-            width: surface_size.width,
-            height: surface_size.height,
-            present_mode,
-        },
-    )
 }
